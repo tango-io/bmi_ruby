38d02dcee2bb2e4970941ddc05b4c4cfcf080638
Narciso Guillen <narciso.guillen@tangosource.com>
2011-11-14 17:51:47 -0600

    Unit testing for CLI

--- test/test_bmi_cli.rb
+++ test/test_bmi_cli.rb
@@ -2,13 +2,32 @@ require File.join(File.dirname(__FILE__), "test_helper.rb")
 require 'bmi/cli'
 
 class TestBmiCli < Test::Unit::TestCase
+  
   def setup
-    Bmi::CLI.execute(@stdout_io = StringIO.new, [])
-    @stdout_io.rewind
-    @stdout = @stdout_io.read
+    Bmi::CLI.calc([])
   end
   
-  def test_print_default_output
-    assert_match(/To update this executable/, @stdout)
+  def test_kilograms
+    data = Bmi::CLI.calc(['-w 80'])
+    kilograms = data[:weight_k].to_f
+    assert_equal(80, kilograms)
+  end
+
+  def test_meters
+    data = Bmi::CLI.calc(['-b 1.80'])
+    meters = data[:height_m].to_f
+    assert_equal(1.80, meters)
+  end
+
+  def test_pounds
+    data = Bmi::CLI.calc(['-W 166'])
+    pounds = data[:weight_p].to_f
+    assert_equal(166, pounds)
+  end
+
+  def test_inches
+    data = Bmi::CLI.calc(['-B 68.11'])
+    inches = data[:height_i].to_f
+    assert_equal(68.11, inches)
   end
 end
